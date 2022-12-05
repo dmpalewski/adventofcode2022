371adb0fa9e15c9299d1d5337c561d8f4d2a4224
dmpalewski <dmpalewski@gmail.com>
2022-12-05 13:36:32 +0000

    completed day 2 part 2

--- day2/rock_paper_scissors.py
+++ day2/rock_paper_scissors.py
@@ -2,6 +2,8 @@ import argparse
 from pathlib import Path
 from enum import IntEnum
 
+from black import out
+
 
 class Outcome(IntEnum):
     WIN = 6
@@ -15,6 +17,16 @@ class Figure(IntEnum):
     SCISSORS = 3
 
 
+winning_figures_map = {
+    Figure.ROCK: Figure.PAPER,
+    Figure.PAPER: Figure.SCISSORS,
+    Figure.SCISSORS: Figure.ROCK
+}
+
+
+losing_figures_map = {v: k for k, v in winning_figures_map.items()}
+
+
 cypher2figure = {
     "A": Figure.ROCK,
     "B": Figure.PAPER,
@@ -25,26 +37,52 @@ cypher2figure = {
 }
 
 
+cypher2outcome = {
+    "X": Outcome.LOSE,
+    "Y": Outcome.DRAW,
+    "Z": Outcome.WIN
+}
+
+
 def get_outcome(opponent_figure: Figure, your_figure: Figure) -> Outcome:
     if opponent_figure == your_figure:
         outcome = Outcome.DRAW
-    elif (
-        (opponent_figure == Figure.ROCK and your_figure == Figure.SCISSORS)
-        or (opponent_figure == Figure.PAPER and your_figure == Figure.ROCK)
-        or (opponent_figure == Figure.SCISSORS and your_figure == Figure.PAPER)
-    ):
+    elif your_figure == losing_figures_map.get(opponent_figure):
         outcome = Outcome.LOSE
     else:
         outcome = Outcome.WIN
     return outcome
 
 
-def process_input(path: Path) -> int:
+def get_your_figure_from_outcome(opponent_figure: Figure, outcome: Outcome) -> Figure:
+    if outcome == Outcome.DRAW:
+        your_figure = opponent_figure
+    elif outcome == Outcome.LOSE:
+        your_figure = losing_figures_map.get(opponent_figure)
+    else:
+        your_figure = winning_figures_map.get(opponent_figure)
+    return your_figure
+
+
+def process_input_pt1(path: Path) -> int:
     total_score = 0
     with open(path, "r") as fin:
         for line in fin.readlines():
             opponent_figure, your_figure = [cypher2figure.get(c) for c in line.strip().split()]
             round_outcome = get_outcome(opponent_figure, your_figure)
+            round_score = round_outcome + your_figure
+            total_score += round_score
+    return total_score
+
+
+def process_input_pt2(path: Path) -> int:
+    total_score = 0
+    with open(path, "r") as fin:
+        for line in fin.readlines():
+            opponent_figure, outcome = [c for c in line.strip().split()]
+            opponent_figure = cypher2figure.get(opponent_figure)
+            round_outcome = cypher2outcome.get(outcome)
+            your_figure = get_your_figure_from_outcome(opponent_figure, round_outcome)
             round_score = round_outcome + your_figure
             total_score += round_score
     return total_score
@@ -58,7 +96,6 @@ if __name__ == "__main__":
         default="day2/input/input.txt",
         help="Path to the input file.",
     )
-
     args = parser.parse_args()
-    total_score = process_input(args.input)
+    total_score = process_input_pt2(args.input)
     print(f"Total score: {total_score}")
