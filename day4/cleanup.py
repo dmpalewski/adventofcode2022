5374f0c3b70a8edf86e3013392626fef165e881c
dmpalewski <dmpalewski@gmail.com>
2022-12-06 19:33:08 +0000

    solution for day 4 part 1

--- day4/cleanup.py
+++ day4/cleanup.py
@@ -1,16 +1,38 @@
 import argparse
 from pathlib import Path
+from dataclasses import dataclass
+from typing import Tuple
 
 
-def is_left_within_right(left, right) -> bool:
-    raise NotImplementedError
+@dataclass
+class Range:
+    lower_bound: int
+    upper_bound: int
+
+
+def get_range_from_string(s: str) -> Range:
+    lo, hi = [int(i) for i in s.strip().split("-")]
+    range = Range(lower_bound=lo, upper_bound=hi)
+    return range
+
+
+def parse_input_line(line: str) -> Tuple[Range, Range]:
+    left_and_right = line.strip().split(",")
+    return tuple(get_range_from_string(s) for s in left_and_right)
+
+
+def is_left_within_right(left: Range, right: Range) -> bool:
+    return right.lower_bound <= left.lower_bound and right.upper_bound >= left.upper_bound
 
 
 def process_input(path: Path) -> int:
     num_fully_contained_pairs = 0
     with open(path, "r") as fin:
         for line in fin.readlines():
-            raise NotImplementedError
+            left, right = parse_input_line(line)
+            is_fully_contained = is_left_within_right(left, right) or is_left_within_right(right, left)
+            if is_fully_contained:
+                num_fully_contained_pairs += 1
     return num_fully_contained_pairs
 
 
