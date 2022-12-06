8420b06f14520b319768791f3f783d8ba1c94504
dmpalewski <dmpalewski@gmail.com>
2022-12-06 20:07:15 +0000

    completed day 4 part 2

--- day4/cleanup.py
+++ day4/cleanup.py
@@ -1,6 +1,6 @@
 import argparse
-from pathlib import Path
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Tuple
 
 
@@ -22,7 +22,17 @@ def parse_input_line(line: str) -> Tuple[Range, Range]:
 
 
 def is_left_within_right(left: Range, right: Range) -> bool:
-    return right.lower_bound <= left.lower_bound and right.upper_bound >= left.upper_bound
+    return (
+        right.lower_bound <= left.lower_bound and right.upper_bound >= left.upper_bound
+    )
+
+
+def is_overlap(left: Range, right: Range) -> bool:
+    return (
+        right.lower_bound <= left.upper_bound and right.lower_bound >= left.lower_bound
+    ) or (
+        right.upper_bound >= left.lower_bound and right.upper_bound <= left.upper_bound
+    )
 
 
 def process_input(path: Path) -> int:
@@ -30,10 +40,27 @@ def process_input(path: Path) -> int:
     with open(path, "r") as fin:
         for line in fin.readlines():
             left, right = parse_input_line(line)
-            is_fully_contained = is_left_within_right(left, right) or is_left_within_right(right, left)
+            is_fully_contained = is_left_within_right(
+                left, right
+            ) or is_left_within_right(right, left)
             if is_fully_contained:
                 num_fully_contained_pairs += 1
     return num_fully_contained_pairs
+
+
+def process_input_pt_2(path: Path) -> int:
+    num_overlaping_pairs = 0
+    with open(path, "r") as fin:
+        for line in fin.readlines():
+            left, right = parse_input_line(line)
+            is_overlaping = (
+                is_overlap(left, right)
+                or is_left_within_right(left, right)
+                or is_left_within_right(right, left)
+            )
+            if is_overlaping:
+                num_overlaping_pairs += 1
+    return num_overlaping_pairs
 
 
 if __name__ == "__main__":
@@ -45,5 +72,5 @@ if __name__ == "__main__":
         help="Path to the input file.",
     )
     args = parser.parse_args()
-    num_fully_contained_pairs = process_input(args.input)
-    print(f"In {num_fully_contained_pairs} pairs one range fully contains the other.")
+    num_fully_contained_pairs = process_input_pt_2(args.input)
+    print(f"Num overlaping pairs {num_fully_contained_pairs}.")
